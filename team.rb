5a5823f4ba6f04f8dc722c6f2c8662914abb3655
noma <noma.developper@gmail.com>
2021-10-27 05:02:03 +0000

    first commit

---
+++ team.rb
@@ -0,0 +1,39 @@
+# クラス定義
+class Team
+
+  # インスタンスが持つ変数（値）
+  attr_accessor :name, :win, :lose, :draw
+
+  # インスタンスを初期化するための、特別なメソッド
+  def initialize(name,win,lose,draw)
+    self.name = name
+    self.win = win
+    self.lose = lose
+    self.draw= draw
+  end
+
+  # インスタンスが持つメソッド（処理）
+  def calc_win_rate
+    self.win.to_f/(self.win+self.lose)
+  end
+  
+  def show_team_result
+    puts "#{self.name}の2020年の成績は#{self.win.to_s}勝#{self.lose.to_s}敗#{self.draw.to_s}分、勝率は#{self.calc_win_rate.to_s}です。"
+  end
+end
+
+# インスタンスの生成と、変数への代入
+sample1 = Team.new('Giants',67,45,8)
+sample2 = Team.new('Tigers',60,53,7)
+sample3 = Team.new('Dragons',60,55,5)
+sample4 = Team.new('BayStars',56,58,6)
+sample5 = Team.new('Carp',52,56,12)
+sample6 = Team.new('Swallows',41,69,10)
+
+# インスタンスの使用
+sample1.show_team_result
+sample2.show_team_result
+sample3.show_team_result
+sample4.show_team_result
+sample5.show_team_result
+sample6.show_team_result
